09b970e2021e5982d9806613f7ca2a20bb4443e8
AzPepoze <pladumguy@gmail.com>
2025-08-26 03:17:31 +0700

    make can use chat

--- src/backend/src/ai/providers/llama-cpp.ts
+++ src/backend/src/ai/providers/llama-cpp.ts
@@ -16,7 +16,7 @@ export class LlamaCPP extends AIProvider {
 	}
 
 	async generateText(history: ChatHistory): Promise<string> {
-		const response = await fetch(`${this.llamaCPPURL}/completion`, {
+		const response = await fetch(`${this.llamaCPPURL}/v1/chat/completions`, {
 			method: "POST",
 			headers: { "Content-Type": "application/json" },
 			body: JSON.stringify({
